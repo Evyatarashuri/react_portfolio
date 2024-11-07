38d85dda31f5143490ba61522d3a2db7ffadc2c3
Evyatar <evyatar54545@gmail.com>
2024-11-07 18:13:58 +0200

    Improvements

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import logo from "../assets/kevinRushLogo.png";
+import logo from "../assets/evyatar-logo.png";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
@@ -7,14 +7,20 @@ const Navbar = () => {
     return (
         <div>
             <nav className="mb-20 flex items-center justify-between py-6 bg-transparent">
-            <div className="flex flex-shrink-0 items-center">
-            <img className="mx-2 w-10" src={logo} alt="logo" />
-            </div>
-            <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-            <FaLinkedin />
-            <FaGithub />
-            <FaInstagram />
-            </div>
+                <div className="flex flex-shrink-0 items-center">
+                    <img className="mx-2 w-10" src={logo} alt="logo" />
+                </div>
+                <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+                    <a href="https://www.linkedin.com/in/evyatar-ashuri-469063237/" target="_blank" rel="noopener noreferrer">
+                        <FaLinkedin />
+                    </a>
+                    <a href="https://github.com/Evyatarashuri" target="_blank" rel="noopener noreferrer">
+                        <FaGithub />
+                    </a>
+                    <a href="https://www.instagram.com/evyatarashuri_/" target="_blank" rel="noopener noreferrer">
+                        <FaInstagram />
+                    </a>
+                </div>
             </nav>
         </div>
     );
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,7 @@
-import project1 from "../assets/projects/project-1.jpg";
-import project2 from "../assets/projects/project-2.jpg";
-import project3 from "../assets/projects/project-3.jpg";
-import project4 from "../assets/projects/project-4.jpg";
+import project1 from "../assets/projects/project-1.png";
+// import project2 from "../assets/projects/project-2.jpg";
+// import project3 from "../assets/projects/project-3.jpg";
+// import project4 from "../assets/projects/project-4.jpg";
 
 export const HERO_CONTENT = `A highly motivated self-learner with excellent communication skills and a strong
 sense of ownership over tasks. I am passionate about software development and
@@ -33,7 +33,12 @@ export const EXPERIENCES = [
     year: " 2024 - 2027",
     role: " bachelor's degree",
     company: " Computer Science",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
+    description: `Applied Object-Oriented Programming (OOP) principles, including polymorphism, in C and
+      Java. Gained experience with data structures such as binary trees and algorithms,
+      focusing on time complexity and optimization. Developed proficiency in C and Java,
+      including memory management with pointers in C and efficient coding practices in Java.
+      Completed various projects that demonstrated practical application and advanced
+      problem-solving abilities.`,
     technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
   },
   // {
@@ -54,37 +59,37 @@ export const EXPERIENCES = [
 
 export const PROJECTS = [
   {
-    title: "E-Commerce Website",
+    title: "ChatConnect",
     image: project1,
     description:
-      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+      "ChatConnect is a full-stack web application built with Django, featuring CRUD operations, user authentication, and a RESTful API for smooth frontend-backend communication. The frontend is developed using Django's built-in template system. The backend utilizes Django ORM for database management, and the entire project is containerized using Docker and orchestrated with Docker Compose. NGINX is configured as a reverse proxy to enhance  production performance. The project showcases scalability and reliability.",
+    technologies: ["HTML", "CSS", "Python", "JavaScript", "REST-API"],
   },
-  {
-    title: "Task Management App",
-    image: project2,
-    description:
-      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-    technologies: ["HTML", "CSS", "Angular", "Firebase"],
-  },
-  {
-    title: "Portfolio Website",
-    image: project3,
-    description:
-      "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "Bootstrap"],
-  },
-  {
-    title: "Blogging Platform",
-    image: project4,
-    description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
-  },
+  // {
+  //   title: "Task Management App",
+  //   image: project2,
+  //   description:
+  //     "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
+  //   technologies: ["HTML", "CSS", "Angular", "Firebase"],
+  // },
+  // {
+  //   title: "Portfolio Website",
+  //   image: project3,
+  //   description:
+  //     "A personal portfolio website showcasing projects, skills, and contact information.",
+  //   technologies: ["HTML", "CSS", "React", "Bootstrap"],
+  // },
+  // {
+  //   title: "Blogging Platform",
+  //   image: project4,
+  //   description:
+  //     "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+  //   technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+  // },
 ];
 
 export const CONTACT = {
   address: "767 Fifth Avenue, New York, NY 10153 ",
   phoneNo: "+12 4555 666 00 ",
-  email: "me@example.com",
+  email: "evyatar54545@gmail.com",
 };
