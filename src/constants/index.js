83c951df9c56637791cb52bd71f05cc593ebdcdb
Evyatar <backend-dev-1@besiness.net>
2025-03-19 22:37:04 +0200

    Improved

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,4 @@
 import project1 from "../assets/projects/project-1.png";
-// import project2 from "../assets/projects/project-2.jpg";
-// import project3 from "../assets/projects/project-3.jpg";
-// import project4 from "../assets/projects/project-4.jpg";
 
 export const HERO_CONTENT = `I'm a driven, self-motivated developer with a strong sense of responsibility and a passion for growth. Always eager to expand my knowledge in software development, I strive to be the best version of myself and view challenges and even mistakes as valuable opportunities for improvement and innovation.`;
 
@@ -12,50 +9,46 @@ export const EXPERIENCES = [
     year: "2024 - Present",
     role: "Backend Engineer",
     company: "BeCompanyLTD",
-    description: ` Backend development using Python and Django, with a focus on building and optimizing RESTful APIs.
-    Designing and implementing API endpoints using Django REST Framework (DRF) for efficient
-    data handling.
-    Managing PostgreSQL databases, including schema design and query optimization.
-    Containerization with Docker and Docker Compose for scalable environments.
-    Implementing CI/CD pipelines in Bitbucket Pipelines using custom YAML configurations.
-    EXPERIENCE
-    Configuring Nginx as a web server, including integration of SSL certificates for secure
-    communication.
-    Working extensively with AWS services, including EC2, S3, RDS, Route 53, Security Groups, and
-    IAM roles management.`,
-    technologies: ["Python", "Django", "AWS", "Docker", "Nginx"],
-  },
+    description: `
+    Experienced in developing and optimizing SaaS and B2B systems using Python, Django, and Django REST Framework (DRF).
+    Designed and implemented RESTful APIs, including CRUD operations, complex backend logic, and integrations with third-party applications via REST and JSON.
+    Developed real-time communication solutions using Django Channels and WebSockets.
 
-  // {
-  //   year: "2021 - 2022",
-  //   role: "Full Stack Developer",
-  //   company: "Facebook",
-  //   description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-  //   technologies: ["Python", "Svelte", "Three.js", "Postgres"],
-  // },
-  // {
-  //   year: "2020 - 2021",
-  //   role: "Software Engineer",
-  //   company: "Paypal",
-  //   description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-  //   technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
-  // },
+    Led a migration from AWS EC2 to ECS, optimizing deployment and reducing costs, while managing Dockerized applications, AWS ALB, IAM roles, and ECS tasks.
+    Developed automation scripts with Boto3 for AWS resource management.
+    Handled Amazon S3 for optimized file storage and delivery.
+    Designed and optimized PostgreSQL schemas, working with Aurora RDS and PGAdmin for efficient database handling.
+
+    Implemented OAuth 2.0, JWT authentication, CSRF protection, and SSL encryption to ensure security.
+    Managed production deployments with Nginx and Gunicorn, ensuring high availability and performance.
+    Built CI/CD pipelines using Bitbucket Pipelines with custom YAML configurations.
+
+    Collaborated closely with frontend developers to integrate backend functionalities seamlessly and enhance user experience.
+    `,
+    technologies: ["Python", "Django", "RESTful APIs", "AWS", "Docker", "Nginx"],
+  },
 ];
+
+
+
+
 
 export const EDUCATION = [
   {
-    year: " 2024 - 2027",
-    role: " bachelor's degree",
-    company: " Computer Science",
-    description: `Applied Object-Oriented Programming (OOP) principles, including polymorphism, in C and
-      Java. Gained experience with data structures such as binary trees and algorithms,
-      focusing on time complexity and optimization. Developed proficiency in C and Java,
-      including memory management with pointers in C and efficient coding practices in Java.
-      Completed various projects that demonstrated practical application and advanced
-      problem-solving abilities.`,
-    technologies: ["Problem-Solving", "Java", "C", "OOP"],
+    year: "2024 - 2027",
+    role: "Bachelor's Degree",
+    company: "Computer Science",
+    description: `
+    Pursuing a Bachelor's degree in Computer Science, where I applied Object-Oriented Programming (OOP) principles, including
+    polymorphism and inheritance, in Java. In C, I gained experience with low-level programming concepts, such as memory management
+    using pointers, working with data structures like binary trees, linked lists, and implementing algorithms with a focus on time
+    complexity and optimization. Developed strong problem-solving skills through various projects that required efficient algorithmic
+    solutions and hands-on implementation of theoretical concepts.
+    `,
+    technologies: ["Problem-Solving", "Java", "C", "OOP", "Algorithms", "Data Structures"],
   }
-]
+];
+
 
 export const PROJECTS = [
   {
@@ -66,31 +59,8 @@ export const PROJECTS = [
     technologies: ["HTML", "CSS", "Python", "JavaScript", "REST-API"],
     link: "https://github.com/Evyatarashuri/ChatConnect",
   },
-  // {
-  //   title: "Task Management App",
-  //   image: project2,
-  //   description:
-  //     "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-  //   technologies: ["HTML", "CSS", "Angular", "Firebase"],
-  // },
-  // {
-  //   title: "Portfolio Website",
-  //   image: project3,
-  //   description:
-  //     "A personal portfolio website showcasing projects, skills, and contact information.",
-  //   technologies: ["HTML", "CSS", "React", "Bootstrap"],
-  // },
-  // {
-  //   title: "Blogging Platform",
-  //   image: project4,
-  //   description:
-  //     "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-  //   technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
-  // },
 ];
 
 export const CONTACT = {
-  // address: "767 Fifth Avenue, New York, NY 10153 ",
-  // phoneNo: "+12 4555 666 00 ",
   email: "evyatar54545@gmail.com",
 };
